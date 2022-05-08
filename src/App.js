6bbd3f9a5d5a3ccd55b760a0a4312361b61f1dac
Nisarg Patel <nisargmpatel73@gmail.com>
2022-05-08 11:15:37 +0530

    Setup eslint, prettier, husky and semantic ui react
    Signed-off-by: Nisarg Patel <nisargmpatel73@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Segment } from 'semantic-ui-react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Segment>Hello World!</Segment>
+    </div>
+  );
+}
+
+export default App;
